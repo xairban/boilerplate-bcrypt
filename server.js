be67b7db30203230777b00ab7390e3210a023418
xairban <157282919+xairban@users.noreply.github.com>
2024-10-28 23:27:31 +0800

    xairban

--- server.js
+++ server.js
@@ -11,28 +11,11 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 bcrypt.hash(myPlaintextPassword, saltRounds, (err,hash) => {
-  if (err) {
-    console.error("Hashing error:", err);
-  } else {
-    console.log("Hash generated:", hash);
+  console.log(hash);
 
-    bcrypt.compare(myPlaintextPassword, hash, (err,result) => {
-      if (err) {
-        console.error("Comparison error:", err);
-      } else {
-        console.log("Password match:", result);
-      }
-    });
-
-   bcrypt.compare(someOtherPlaintextPassword, hash, (err,result) => {
-      if (err) {
-        console.error("Comparison error:", err);
-      } else {
-        console.log("Password match:", result);
-      }
-    });
-
-  }
+  bcrypt.compare(myPlaintextPassword, hash, (err,result) => {
+    console.log("Password match:", result);
+  });
 });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
